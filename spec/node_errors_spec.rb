35cc54f5feb2018e83a24d0c3a2e8727642661fd
Andreas Robecke <a.robecke@getsafe.de>
2020-02-24 22:46:47 +0100

    Test multiple hash schemas

--- spec/node_errors_spec.rb
+++ spec/node_errors_spec.rb
@@ -124,7 +124,77 @@ RSpec.describe NxtSchema do
       end
 
       context 'when the array node allows multiple schemas for items' do
+        subject do
+          NxtSchema.new do |root|
+            root.schema(:company) do |company|
+              company.nodes(:workers) do |workers|
+                workers.schema(:employee) do |employee|
+                  employee.requires(:first_name, :String)
+                  employee.requires(:last_name, :String)
+                end
 
+                workers.schema(:boss) do |boss|
+                  boss.requires(:title, :String)
+                  boss.requires(:last_name, :String)
+                end
+              end
+            end
+          end
+        end
+
+        context 'when there are invalid nodes' do
+          let(:schema) do
+            {
+              company: {
+                workers: [
+                  { first_name: 'Andy', last_name: 'Robecke' },
+                  { first_name: 'Rapha', last_name: 'Kallensee' },
+                  { title: 'CTO', last_name: 'Blaesing' },
+                  { title: 'CEO', last_name: 'Wienz' },
+                  { title: nil },
+                  { },
+                  'Lütfi'
+                ]
+              }
+            }
+          end
+
+          it do
+            subject.apply(schema)
+
+            expect(subject.errors).to eq(
+              "root.company.workers.4.employee"=>["Required key :first_name is missing in {:title=>nil}", "Required key :last_name is missing in {:title=>nil}"],
+              "root.company.workers.4.boss"=>["Required key :last_name is missing in {:title=>nil}"],
+              "root.company.workers.4.boss.title"=>["nil violates constraints (type?(String, nil) failed)"],
+              "root.company.workers.5.employee"=>["Required key :first_name is missing in {}", "Required key :last_name is missing in {}"],
+              "root.company.workers.5.boss"=>["Required key :title is missing in {}", "Required key :last_name is missing in {}"],
+              "root.company.workers.6.employee"=>["\"Lütfi\" violates constraints (type?(Hash, \"Lütfi\") failed)"],
+              "root.company.workers.6.boss"=>["\"Lütfi\" violates constraints (type?(Hash, \"Lütfi\") failed)"]
+            )
+          end
+        end
+
+        context 'when all nodes are valid' do
+          let(:schema) do
+            {
+              company: {
+                workers: [
+                  { first_name: 'Andy', last_name: 'Robecke' },
+                  { first_name: 'Rapha', last_name: 'Kallensee' },
+                  { title: 'CTO', last_name: 'Blaesing' },
+                  { title: 'CEO', last_name: 'Wienz' }
+                ]
+              }
+            }
+          end
+
+          it do
+            subject.apply(schema)
+            expect(subject).to be_valid
+            expect(subject.errors).to be_empty
+            expect(subject.value_store).to eq(schema)
+          end
+        end
       end
     end
   end
