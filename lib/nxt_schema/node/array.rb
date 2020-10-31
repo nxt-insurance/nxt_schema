ca60fc1718721450d6dcce29bd7c8b4e644872bb
Andreas Robecke <a.robecke@getsafe.de>
2020-10-31 15:05:07 +0100

    Move any_of and all_of in has_sub_nodes module

--- lib/nxt_schema/node/array.rb
+++ lib/nxt_schema/node/array.rb
@@ -8,28 +8,6 @@ module NxtSchema
       def initialize(name:, value_type: DEFAULT_TYPE, parent_node:, **options, &block)
         super
       end
-
-      # This allows to validate against multiple sub schemas
-      # TODO: This should also exist in hash
-      def any_of(&block)
-        @all_of = false
-        @any_of = true
-
-        configure(&block)
-        self
-      end
-
-      def all_of(&block)
-        @any_of = false
-        @all_of = true
-
-        configure(&block)
-        self
-      end
-
-      def sub_nodes_evaluation?(value)
-        value == (@all_of ? :all : :any)
-      end
     end
   end
 end
--- lib/nxt_schema/node/has_sub_nodes.rb
+++ lib/nxt_schema/node/has_sub_nodes.rb
@@ -30,6 +30,26 @@ module NxtSchema
         @sub_nodes ||= Node::SubNodes.new
       end
 
+      def any_of(&block)
+        @all_of = false
+        @any_of = true
+
+        configure(&block)
+        self
+      end
+
+      def all_of(&block)
+        @any_of = false
+        @all_of = true
+
+        configure(&block)
+        self
+      end
+
+      def sub_nodes_evaluation?(value)
+        value == (@all_of ? :all : :any)
+      end
+
       private
 
       def build_leaf_node(name, type, **options, &block)
