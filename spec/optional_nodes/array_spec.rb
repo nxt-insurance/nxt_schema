45f8da9bf086929ad67a82bf660975ed3bb3ba3e
Andreas Robecke <a.robecke@getsafe.de>
2020-11-01 12:39:00 +0100

    optional nodes specs

---
+++ spec/optional_nodes/array_spec.rb
@@ -0,0 +1,38 @@
+RSpec.describe NxtSchema do
+  subject { schema.apply(input) }
+
+  context 'when nodes are optional' do
+    let(:schema) do
+      NxtSchema.hash(:person) do |person|
+        person.array(:skills).optional do |skills|
+          skills.node(:skill, :String)
+        end
+      end
+    end
+
+
+    context 'and it is present' do
+      let(:input) do
+        { skills: ['Ruby', 'Javascript'] }
+      end
+
+      it { expect(subject).to be_valid }
+
+      it do
+        expect(subject.output).to eq(input)
+      end
+    end
+
+    context 'and it is not present' do
+      let(:input) do
+        { }
+      end
+
+      it { expect(subject).to be_valid }
+
+      it do
+        expect(subject.output).to eq(input)
+      end
+    end
+  end
+end
---
+++ spec/optional_nodes/leaf_spec.rb
@@ -0,0 +1,45 @@
+RSpec.describe NxtSchema do
+  subject { schema.apply(input) }
+
+  context 'when nodes are optional' do
+    let(:schema) do
+      NxtSchema.hash(:person) do |person|
+        person.node(:first_name, :String)
+        person.node(:last_name, :String)
+        person.node(:phone, :String).optional
+      end
+    end
+
+
+    context 'and it is present' do
+      let(:input) do
+        {
+          first_name: 'Hanna',
+          last_name: 'Robecke',
+          phone: '017696426299'
+        }
+      end
+
+      it { expect(subject).to be_valid }
+
+      it do
+        expect(subject.output).to eq(input)
+      end
+    end
+
+    context 'and it is not present' do
+      let(:input) do
+        {
+          first_name: 'Hanna',
+          last_name: 'Robecke'
+        }
+      end
+
+      it { expect(subject).to be_valid }
+
+      it do
+        expect(subject.output).to eq(input)
+      end
+    end
+  end
+end
