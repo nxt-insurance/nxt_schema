5986bb48234f681737e5079434c5b3a59da36818
Andreas Robecke <a.robecke@getsafe.de>
2020-11-05 20:55:15 +0000

    Add path method

--- lib/nxt_schema/node/base.rb
+++ lib/nxt_schema/node/base.rb
@@ -8,6 +8,7 @@ module NxtSchema
         @level = parent_node ? parent_node.level + 1 : 0
         @is_root = parent_node.nil?
         @root = parent_node.nil? ? self : parent_node.root
+        @path = resolve_path
 
         resolve_optional_option
         resolve_omnipresent_option
@@ -19,7 +20,7 @@ module NxtSchema
       end
 
       attr_accessor :name, :parent_node, :options, :type, :level, :root, :additional_keys_strategy
-      attr_reader :type_system
+      attr_reader :type_system, :path
 
       # This does not work with keyword args?!
       def apply(input = MissingInput, context = nil, parent = nil)
@@ -44,7 +45,7 @@ module NxtSchema
 
       private
 
-      attr_accessor :optional
+      attr_writer :path
 
       def resolve_type(name_or_type)
         @type = root.send(:type_resolver).resolve(type_system, name_or_type)
@@ -94,6 +95,10 @@ module NxtSchema
         raise Errors::InvalidOptions, "Can't make omnipresent node optional" if optional? && omnipresent
 
         @omnipresent = omnipresent
+      end
+
+      def resolve_path
+        self.path = root? ? name : "#{parent_node.path}.#{name}"
       end
     end
   end
