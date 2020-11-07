d8af2edd2422f30f38eada5b25ffc321a8a93628
Andreas Robecke <a.robecke@getsafe.de>
2020-11-07 13:35:33 +0000

    Rename index to error key

--- lib/nxt_schema/application/errors.rb
+++ lib/nxt_schema/application/errors.rb
@@ -1,7 +1,7 @@
 module NxtSchema
   module Application
     class Errors
-      DEFAULT_ERROR_INDEX = :itself
+      DEFAULT_ERROR_KEY = :itself
 
       def initialize(application:, node:)
         @application = application
@@ -21,28 +21,28 @@ module NxtSchema
 
       alias to_h all
 
-      def add_schema_error(error, index: DEFAULT_ERROR_INDEX)
+      def add_schema_error(error, index: DEFAULT_ERROR_KEY)
         schema_errors_store(index) << error
       end
 
-      def add_validation_error(error, index: DEFAULT_ERROR_INDEX)
+      def add_validation_error(error, index: DEFAULT_ERROR_KEY)
         validation_errors_store(index) << error
       end
 
       def merge_schema_errors(child_application)
         errors = child_application.schema_errors
-        index = child_application.error_key
+        child_error_key = child_application.error_key
 
         if errors.is_a?(::Hash)
-          if errors.keys == [DEFAULT_ERROR_INDEX]
-            schema_errors[index] = errors.fetch(DEFAULT_ERROR_INDEX) + schema_errors.fetch(index, [])
+          if errors.keys == [DEFAULT_ERROR_KEY]
+            schema_errors[child_error_key] = errors.fetch(DEFAULT_ERROR_KEY) + schema_errors.fetch(child_error_key, [])
           else
-            schema_errors[index] ||= {}
-            schema_errors[index].merge!(errors)
+            schema_errors[child_error_key] ||= {}
+            schema_errors[child_error_key].merge!(errors)
           end
         else
-          schema_errors[index] ||= []
-          schema_errors[index] << errors
+          schema_errors[child_error_key] ||= []
+          schema_errors[child_error_key] << errors
         end
       end
 
@@ -50,12 +50,12 @@ module NxtSchema
         schema_errors.any? || validation_errors.any?
       end
 
-      def schema_errors_store(index)
-        schema_errors[index] ||= []
+      def schema_errors_store(error_key)
+        schema_errors[error_key] ||= []
       end
 
-      def validation_errors_store(index)
-        validation_errors[index] ||= []
+      def validation_errors_store(error_key)
+        validation_errors[error_key] ||= []
       end
     end
   end
