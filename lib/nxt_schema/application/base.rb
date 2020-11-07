12110db796a1688886fb21b4703a1d9eaee0760c
Andreas Robecke <a.robecke@getsafe.de>
2020-11-07 15:12:47 +0000

    resolve nested error key

--- lib/nxt_schema/application/base.rb
+++ lib/nxt_schema/application/base.rb
@@ -9,10 +9,12 @@ module NxtSchema
         @error_key = error_key
         @errors = Errors.new(application: self, node: node)
         @context = context || parent&.context
+
+        resolve_nested_error_key
       end
 
       attr_accessor :output, :node, :input
-      attr_reader :parent, :errors, :context, :error_key
+      attr_reader :parent, :errors, :context, :error_key, :nested_error_key
 
       def call
         raise NotImplementedError, 'Implement this in our sub class'
@@ -38,6 +40,22 @@ module NxtSchema
         self.output = output
       rescue Dry::Types::ConstraintError, Dry::Types::CoercionError => error
         add_schema_error(error.message)
+      end
+
+      def resolve_nested_error_key
+        parts = []
+
+        if parent
+          parts << parent.nested_error_key
+        else
+          parts << name
+        end
+
+        parts << node.name if error_key.is_a?(Integer)
+        parts << error_key
+        parts.compact!
+        parts.reject! { |part| part == Application::Errors::DEFAULT_ERROR_KEY }
+        @nested_error_key = parts.join('.')
       end
     end
   end
