9754c0dc93cf9a7ae0086fd34db769f655cfc0bd
Andreas Robecke <a.robecke@getsafe.de>
2020-11-13 17:22:24 +0000

    Make spec green

--- spec/node/has_sub_nodes/schema_spec.rb
+++ spec/node/has_sub_nodes/schema_spec.rb
@@ -130,22 +130,14 @@ RSpec.describe NxtSchema do
       it { expect(subject).to_not be_valid }
 
       it 'returns the correct errors' do
-        expect(subject.errors).to eq(
-          {
-            itself: ["The following keys are missing: [:name]"],
-            name: ["NxtSchema::MissingInput violates constraints (type?(String, NxtSchema::MissingInput) failed)"],
-            houses: {
-              0 => {
-                itself: ["The following keys are missing: [:zip_code]"],
-                zip_code: ["NxtSchema::MissingInput violates constraints (type?(Integer, NxtSchema::MissingInput) failed)"]
-              },
-              1 => { street: ["nil violates constraints (type?(String, nil) failed)"] },
-              2 => {
-                street: ["1 violates constraints (type?(String, 1) failed)"],
-                zip_code: ["\"67661\" violates constraints (type?(Integer, \"67661\") failed)"]
-              }
-            }
-          }
+        expect(subject.errors[:schema_errors]).to eq(
+          'person' => ['The following keys are missing: [:name]'],
+          'person.houses.house[0]' => ['The following keys are missing: [:zip_code]'],
+          'person.houses.house[0].zip_code' => ['NxtSchema::MissingInput violates constraints (type?(Integer, NxtSchema::MissingInput) failed)'],
+          'person.houses.house[1].street' => ['nil violates constraints (type?(String, nil) failed)'],
+          'person.houses.house[2].street' => ['1 violates constraints (type?(String, 1) failed)'],
+          'person.houses.house[2].zip_code' => ['\'67661\' violates constraints (type?(Integer, \'67661\') failed)'],
+          'person.name' => ['NxtSchema::MissingInput violates constraints (type?(String, NxtSchema::MissingInput) failed)']
         )
       end
     end
